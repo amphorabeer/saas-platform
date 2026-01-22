edefc7d33660adebf20b602624b086c7f61aec1f
შენი სახელი <@email.com>
2026-01-22 15:21:55 +0100

    Fix Prisma import paths in Hotel app

--- apps/hotel/src/pages/api/auth/[...nextauth].ts
+++ apps/hotel/src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 import bcrypt from "bcryptjs"
-import { PrismaClient } from '../../../../node_modules/.prisma/client'
+import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
